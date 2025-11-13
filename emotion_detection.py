b03dc3ca32a3da615289edc41f5a6317ea4d524b
AlyKhalil <alywalaa@gmail.com>
2025-11-12 22:08:56 -0500

    Second Commit

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,10 +1,19 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse):
     base_url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     json_obj = { "raw_document": { "text": text_to_analyse } }
 
-    response  = requests.post(base_url, json = json_obj, headers = headers)
+    response = requests.post(base_url, json = json_obj, headers = header)
+    formatted_response = json.loads(response.text)['emotionPredictions'][0]['emotion']
 
-    return response.text
+    return{
+        'anger': formatted_response['anger'],
+        'disgust': formatted_response['disgust'],
+        'fear': formatted_response['fear'],
+        'joy': formatted_response['joy'],
+        'sadness': formatted_response['sadness'],
+        'dominant_emotion': max(formatted_response, key=formatted_response.get)
+    }
